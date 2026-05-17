c30badaca5b1baf9301109ad99245c92cb2366f6
Nischal Boj <nischalboj@gmail.com>
2026-05-17 20:57:35 +0545

    may 17

--- may17.py
+++ may17.py
@@ -3,7 +3,42 @@ import numpy as np
 # print(npArr/npArr)
 
 #2d array multiplication 
-npArr2d = np.arange(1,9).reshape(2,4)
-print(npArr2d)
-nparr3 = np.arange(1,9).reshape(4,2)
-print(nparr3@npArr2d)
+# npArr2d = np.arange(1,9).reshape(2,4)
+# print(npArr2d)
+# nparr3 = np.arange(1,9).reshape(4,2)
+# print(nparr3@npArr2d)
+
+import pandas as pd
+# labels = ["anish", "nama"]
+# labels2 = ["anish", "naman"]
+# myList = [1,4]
+# myList2 = [1,5]
+# print(pd.Series(labels))
+
+
+# familiarity_with_python = {'anish':1, 'naman':2}
+# print(pd.Series(data = labels, index= myList))
+# print(pd.Series(data = labels2, index= myList2))
+# check = pd.Series(data = labels, index= myList)
+# check2 = pd.Series(data = labels2, index= myList2)
+
+
+# print(pd.Series(familiarity_with_python))
+# print(check+check2)
+# print(check)
+# print(check2)
+
+data = {
+    "name": ["Ram", "Sita"],
+    "age": [25,1],
+    "city": ["Kathmandu", "Lalitpur"]
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df[df["age"] > 20])
+df['nameAddress']  =df['name'] + df['city']
+print(df)
+print(df['age']>20)
+print(df[df['nameAddress']])
+# to check = df.drop, df.drop implicit something, df.loc
---
+++ test.py
@@ -0,0 +1,2 @@
+check = range(1,10)
+print(type (check))
